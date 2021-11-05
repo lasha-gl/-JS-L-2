81fae4372f5d9dab8f5ed809b1d69274378ccff5
lasha-gl <gegechkori19@gmail.com>
2021-11-05 19:35:53 +0400

    add

---
+++ script.js
@@ -0,0 +1,62 @@
+// #1
+
+// function sum1 (...args) {
+//     let sum2= 0;
+//     for (let x of args){
+//         if (x > 0) {
+//             sum2+= x;
+//         }
+//     }
+//     return sum2;
+// }
+// let result = sum1 (2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+
+// console.log(result);
+
+
+// #2
+
+// function sum (...numbers) {
+//     let numberssum= 0;
+//     for (let item of numbers){
+//         numberssum += item;
+//     }
+//     console.log(numberssum);
+// }
+// sum (10, 50, 6, 7, 8, 11, 6, 3, 9);
+
+
+// #3
+
+// function printName(){
+//     let user = {
+//         firstname: 'giorgi',
+//         lastname: 'saakadze',
+//         age: 32,
+//         isloggedin: true
+//       }
+//     if(user.isloggedin == true){
+//         console.log(user.firstname + ' ' + user.lastname); 
+//     }
+    
+// }
+
+// let result = printName();
+
+
+// #4
+
+// numbers = [10, 20, 30, 40, -40, -30, -20, -10];
+
+// function max(numbers){
+//     let zero = 0;
+//     for(let items of numbers){
+//         if(items > zero){
+//             zero = items;
+//         }    
+//     }    
+//     return zero;
+// }    
+
+// let result = max(numbers)
+// console.log(result);
